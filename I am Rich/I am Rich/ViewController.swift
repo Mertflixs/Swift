e6281733302e8d3b5983fc7558c4c2de061e5364
mertflixs <mertflix@outlook.com>
2022-11-24 20:16:10 +0300

    First_Swift_Poject

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Mert Alaşahan on 23.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
