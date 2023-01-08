cb2c7b698480ca4e011e764c1de6e7ab89cd70bd
mertflixs <mertflix@outlook.com>
2023-01-08 16:43:56 +0300

    quizler_app

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,4 +1,36 @@
-import UIKit
+struct Town {
+    let name = "Town"
+    var citizens = ["mert","hakan","burak","eyup"]
+    var resources = ["Grain": 100, "Ore": 42, "Wood": 75]
+    
+    func fortify() {
+        print("Defences increased!")
+    }
+}
 
-var greeting = "Hello, playground"
-//print(greeting)
+struct newTown {
+    let name: String
+    var citizens: [String]
+    var resources: [String: Int]
+    
+    init(name: String, citizens: [String], resources: [String: Int]) {
+        self.name = name
+        self.citizens = citizens
+        self.resources = resources
+    }
+}
+
+var anotherTown = newTown(name: "Nameless Island", citizens: ["Tom Hanks"], resources: ["Coconut": 100])
+anotherTown.citizens.append("Mertflix")
+print(anotherTown.citizens)
+print(anotherTown.resources)
+
+var myTown = Town()
+
+print(myTown.name)
+print("\(myTown.name) has \(myTown.resources["Wood"]!) bags of Wood!")
+
+myTown.citizens.append("Yaratık")
+print(myTown.citizens.count)
+
+myTown.fortify()
