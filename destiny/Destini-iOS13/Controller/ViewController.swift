3921f4f7f77e7228e1406ebb7a81b7deab4ee8ba
mertflixs <mertflix@outlook.com>
2023-01-11 19:28:42 +0300

    update

--- destiny/Destini-iOS13/Controller/ViewController.swift
+++ destiny/Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let story = "You see a fork in the road"
+    let choice1 = "Take a left."
+    let choice2 = "Take a right."
+    
+    var brainStory = storyBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
-
-
+    
+    @IBAction func clickedButton(_ sender: UIButton) {
+        let answer = sender.currentTitle!
+        
+        if (brainStory.getCounter() + 1 < brainStory.Story.count) {
+            if (answer == brainStory.getChoice1()) {
+                brainStory.counterPlus()
+                updateUI()
+            } else {
+                brainStory.counterPlus()
+                updateUI()
+            }
+        }
+        else {
+            print("bitti kanks")
+            storyLabel.text = "Bitti"
+            choice1Button.isHidden = true
+            choice2Button.isHidden = true
+        }
+    }
+    
+    @objc func updateUI() {
+        storyLabel.text = brainStory.getStory()
+        choice1Button.setTitle(brainStory.getChoice1(), for: .normal)
+        choice2Button.setTitle(brainStory.getChoice2(), for: .normal)
+    }
+    
+    @objc func lastUpdate() {
+        
+    }
+    
 }
 
--- destiny/Destini-iOS13/Model/StoryBrain.swift
+++ destiny/Destini-iOS13/Model/StoryBrain.swift
@@ -8,4 +8,34 @@
 
 import Foundation
 
+struct storyBrain {
+    var counter = 0
+    
+    let Story = [
+        story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+        story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+        story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
+    ]
 
+    func getStory () -> String {
+        return Story[counter].title
+    }
+    
+    func getChoice1 () -> String {
+        return Story[counter].choice1
+    }
+    
+    func getChoice2 () -> String {
+        return Story[counter].choice2
+    }
+    
+    func getCounter () -> Int {
+        return counter
+    }
+    
+    mutating func counterPlus () {
+        if (counter < Story.count) {
+            counter += 1
+        }
+    }
+}
